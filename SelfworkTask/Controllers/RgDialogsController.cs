60dcadaed5b7d8aae4ecaeb2846882d4c70e8f24
WOTTAKWOT <aywee26@gmail.com>
2022-08-23 15:29:46 +0300

    Implement RgDialogsController.

---
+++ SelfworkTask/Controllers/RgDialogsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SelfworkTask.Services;
+
+namespace SelfworkTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RgDialogsController : ControllerBase
+    {
+        private readonly IDialogSearchService _dialogSearchservice;
+
+        public RgDialogsController(IDialogSearchService dialogSearchservice)
+        {
+            _dialogSearchservice = dialogSearchservice;
+        }
+
+        [HttpGet("dialog")]
+        public Guid Dialog([FromQuery]List<Guid> clients)
+        {
+            return _dialogSearchservice.SearchDialog(clients);
+        }
+    }
+}
