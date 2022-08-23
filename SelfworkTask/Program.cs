b2ed5ce202479db7e9d6b4b2488f77419e6784e2
WOTTAKWOT <aywee26@gmail.com>
2022-08-23 15:40:09 +0300

    Implement Program.

--- SelfworkTask/Program.cs
+++ SelfworkTask/Program.cs
@@ -1,25 +1,14 @@
+using SelfworkTask.Services;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
+builder.Services.AddScoped<IDialogSearchService, DialogSearchService>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
