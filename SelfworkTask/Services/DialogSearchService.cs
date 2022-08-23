a741ad782517c032c0a81b12c2816821af6a3c9b
WOTTAKWOT <aywee26@gmail.com>
2022-08-23 15:23:06 +0300

    Implement DialogSearchService.

---
+++ SelfworkTask/Services/DialogSearchService.cs
@@ -0,0 +1,33 @@
+﻿using Ardalis.GuardClauses;
+using SelfworkTask.Models;
+
+namespace SelfworkTask.Services;
+
+public class DialogSearchService : IDialogSearchService
+{
+    private readonly List<RGDialogsClients> _dialogsClients;
+
+    public DialogSearchService()
+    {
+        _dialogsClients = new RGDialogsClients().Init();
+    }
+
+    public Guid SearchDialog(List<Guid> clients)
+    {
+        Guard.Against.Null(clients);
+
+        if (clients.Count == 0)
+            return Guid.Empty;
+
+        var clientDialogLookup = _dialogsClients
+            .ToLookup(list => list.IDClient, list => list.IDRGDialog);
+
+        var result = clientDialogLookup[clients[0]];
+        for (int i = 1; i < clients.Count; i++)
+        {
+            result = result.Intersect(clientDialogLookup[clients[i]]);
+        }
+
+        return result.FirstOrDefault();
+    }
+}
---
+++ SelfworkTask/Services/IDialogSearchService.cs
@@ -0,0 +1,6 @@
+﻿namespace SelfworkTask.Services;
+
+public interface IDialogSearchService
+{
+    Guid SearchDialog(List<Guid> clients);
+}
